85855ae55b80600991a737e262c8bf3deb0db6f9
niku <niku@niku.name>
2019-06-15 14:35:55 +0900

    三角測量に使ったけどもう使わないので消した

--- test/tddbc2019sapporo_test.exs
+++ test/tddbc2019sapporo_test.exs
@@ -12,11 +12,6 @@ defmodule Tddbc2019sapporoTest do
       assert ClosedRange.lower_point(closed_range) == {:ok, 3}
     end
 
-    test "下端点が5のとき、5が取得できる" do
-      closed_range = ClosedRange.new(5, 10)
-      assert ClosedRange.lower_point(closed_range) == {:ok, 5}
-    end
-
     test "ClosedRange以外の値が与えられた時lower_pointはエラーを返す" do
       assert ClosedRange.lower_point(5) == :error
     end
@@ -26,11 +21,6 @@ defmodule Tddbc2019sapporoTest do
     test "上端点が3のとき、3が取得できる" do
       closed_range = ClosedRange.new(1, 3)
       assert ClosedRange.upper_point(closed_range) == {:ok, 3}
-    end
-
-    test "上端点が5のとき、5が取得できる" do
-      closed_range = ClosedRange.new(1, 5)
-      assert ClosedRange.upper_point(closed_range) == {:ok, 5}
     end
 
     test "ClosedRange以外の値が与えられた時upper_pointはエラーを返す" do
