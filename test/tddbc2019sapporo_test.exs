9470548ea0d769f5044304d60936f870a01493bc
niku <niku@niku.name>
2019-06-15 15:48:28 +0900

    他のテストで作成のところを包含しているので消した

--- test/tddbc2019sapporo_test.exs
+++ test/tddbc2019sapporo_test.exs
@@ -1,10 +1,6 @@
 defmodule Tddbc2019sapporoTest do
   use ExUnit.Case
 
-  test "閉区間を作ることができる" do
-    assert ClosedRange.new(3, 5)
-  end
-
   describe "下端点はlower_pointで取得できる" do
     test "下端点が3のとき、3が取得できる" do
       closed_range = ClosedRange.new(3, 10)
