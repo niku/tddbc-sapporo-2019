6b774cb1e4a815656c39d27cb240aaab7d820748
niku <niku@niku.name>
2019-06-15 13:58:43 +0900

    下端点を取得できた

--- lib/closed_range.ex
+++ lib/closed_range.ex
@@ -4,4 +4,8 @@ defmodule ClosedRange do
   def new(lower, upper) do
     struct!(ClosedRange, lower: lower, upper: upper)
   end
+
+  def lower_point(closed_range) do
+    3
+  end
 end
--- test/tddbc2019sapporo_test.exs
+++ test/tddbc2019sapporo_test.exs
@@ -5,4 +5,9 @@ defmodule Tddbc2019sapporoTest do
   test "閉区間を作ることができる" do
     assert ClosedRange.new(3, 5)
   end
+
+  test "閉区間の下端点を取得する" do
+    closed_range = ClosedRange.new(3, 5)
+    assert ClosedRange.lower_point(closed_range) == 3
+  end
 end
